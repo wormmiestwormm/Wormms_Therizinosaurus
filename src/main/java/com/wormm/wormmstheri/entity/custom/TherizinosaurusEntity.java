616d32764b42372a7fa31e5da5e657c47d73d0cc
wormmiestwormm <110218392+wormmiestwormm@users.noreply.github.com>
2025-08-20 16:34:34 -0700

    Added Therizinosaurus fighting back and taming

--- src/main/java/com/wormm/wormmstheri/entity/custom/TherizinosaurusEntity.java
+++ src/main/java/com/wormm/wormmstheri/entity/custom/TherizinosaurusEntity.java
@@ -1,6 +1,7 @@
 package com.wormm.wormmstheri.entity.custom;
 
 import com.wormm.wormmstheri.entity.ModEntities;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -9,6 +10,8 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.TimeUtil;
 import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -20,31 +23,30 @@ import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.loot.LootParams;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
 
-public class TherizinosaurusEntity extends Animal /* NeutralMob*/ {
-    //private static final EntityDataAccessor<Integer> DATA_REMAINING_ANGER_TIME = SynchedEntityData.defineId(TherizinosaurusEntity.class, EntityDataSerializers.INT);
+public class TherizinosaurusEntity extends TamableAnimal implements NeutralMob {
     public final AnimationState idleAnimationState = new AnimationState();
+    public final AnimationState attackAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
-    //private UUID persistentAngerTarget;
 
-    //private static final UniformInt PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(20, 39);
-
-
-    public TherizinosaurusEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
+    public TherizinosaurusEntity(EntityType<? extends TamableAnimal> pEntityType, Level pLevel) {
         super (pEntityType, pLevel);
     }
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        //this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0, true));
+        this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0, true));
         this.goalSelector.addGoal(2, new MoveTowardsTargetGoal(this, 0.9, 32.0F));
         this.goalSelector.addGoal(3, new BreedGoal(this, 1.0));
         this.goalSelector.addGoal(4, new TemptGoal(this, 1.25, stack -> stack.is(Items.POTATO), false));
@@ -52,16 +54,16 @@ public class TherizinosaurusEntity extends Animal /* NeutralMob*/ {
         this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0));
         this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6.0F));
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
-/*
+
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isAngryAt));
-        this.targetSelector.addGoal(3, new ResetUniversalAngerTargetGoal<>(this, true));*/
+        this.targetSelector.addGoal(3, new ResetUniversalAngerTargetGoal<>(this, true));
     }
 
     public static AttributeSupplier.Builder createAttributes(){
         return Animal.createLivingAttributes()
             .add(Attributes.MAX_HEALTH, 150)
-                .add(Attributes.MOVEMENT_SPEED, 0.20)
+                .add(Attributes.MOVEMENT_SPEED, 0.35)
                 .add(Attributes.FOLLOW_RANGE, 100)
                 .add(Attributes.KNOCKBACK_RESISTANCE, 1.0)
                 .add(Attributes.ATTACK_DAMAGE, 20.0);
@@ -85,6 +87,49 @@ public class TherizinosaurusEntity extends Animal /* NeutralMob*/ {
             --this.idleAnimationTimeout;
         }
     }
+
+    @Override
+    public InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        Item item = itemstack.getItem();
+        if (!this.level().isClientSide || this.isBaby() && this.isFood(itemstack)) {
+            if (this.isTame()) {
+                if (this.isFood(itemstack) && this.getHealth() < this.getMaxHealth()) {
+                    itemstack.consume(1, pPlayer);
+                    FoodProperties foodproperties = itemstack.get(DataComponents.FOOD);
+                    float f = foodproperties != null ? (float)foodproperties.nutrition() : 1.0F;
+                    this.heal(2.0F * f);
+                    return InteractionResult.sidedSuccess(this.level().isClientSide());
+
+                } else {
+                    return super.mobInteract(pPlayer, pHand);
+                }
+
+            } else if (itemstack.is(Items.POTATO)) {
+                itemstack.consume(1, pPlayer);
+                this.tryToTame(pPlayer);
+                return InteractionResult.SUCCESS;
+
+            } else {
+                return super.mobInteract(pPlayer, pHand);
+            }
+
+        } else {
+            return InteractionResult.sidedSuccess(this.level().isClientSide);
+        }
+    }
+
+    private void tryToTame(Player pPlayer) {
+        if (this.random.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, pPlayer)) {
+            this.tame(pPlayer);
+            this.navigation.stop();
+            this.setTarget(null);
+            this.level().broadcastEntityEvent(this, (byte)7);
+        } else {
+            this.level().broadcastEntityEvent(this, (byte)6);
+        }
+    }
+
 
     @Override
     public void tick() {
@@ -116,31 +161,28 @@ public class TherizinosaurusEntity extends Animal /* NeutralMob*/ {
         return 2;
     }
 
-
-/*
     @Override
     public int getRemainingPersistentAngerTime() {
-        return this.entityData.get(DATA_REMAINING_ANGER_TIME);
+        return 0;
     }
 
     @Override
-    public void setPersistentAngerTarget(@Nullable UUID pTarget) {
-        this.persistentAngerTarget = pTarget;
-    }
-    @Override
-    public void setRemainingPersistentAngerTime(int pTime) {
-        this.entityData.set(DATA_REMAINING_ANGER_TIME, pTime);
+    public void setRemainingPersistentAngerTime(int pRemainingPersistentAngerTime) {
+
     }
 
-    @Nullable
     @Override
-    public UUID getPersistentAngerTarget() {
-        return this.persistentAngerTarget;
+    public @org.jetbrains.annotations.Nullable UUID getPersistentAngerTarget() {
+        return null;
+    }
+
+    @Override
+    public void setPersistentAngerTarget(@org.jetbrains.annotations.Nullable UUID pPersistentAngerTarget) {
+
     }
 
     @Override
     public void startPersistentAngerTimer() {
-        this.setRemainingPersistentAngerTime(PERSISTENT_ANGER_TIME.sample(this.random));
+
     }
-*/
 }
